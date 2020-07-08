f81bae0fe11dba05bab7641903da57817b063d18
Sharansrj567 <54804304+Sharansrj567@users.noreply.github.com>
2020-07-08 18:47:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setcourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setcourseGoals(currentGoal => [...currentGoal, { key: Math.random().toString(), value: goalTitle }]);
+  };
+
+
+  return (
+    <View style={styles.viewer}>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList
+      keyExtractor={(item,index)=> item.id}
+      data={courseGoals} renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewer: {
+    padding: 20,
+  }
+});
